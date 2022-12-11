02df26fe7c27086e01a5feb766dd875b2cd323fd
“steven1303” <steven.grafensen@gmail.com>
2022-12-12 05:51:28 +0700

    first commit

---
+++ src/AdminServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Steven1303\AdminPanel;
+
+use Illuminate\Support\ServiceProvider;
+
+class AdminServiceProvider extends ServiceProvider{
+
+    public function boot(){
+        $this->loadRoutesFrom(__DIR__.'/routes/admin.php');
+        $this->loadViewsFrom(__DIR__.'/views','admin');
+    }
+
+    public function register(){
+        
+    }
+}
