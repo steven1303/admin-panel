a3eebd7a9fa066343ef4ad89f2610d138b76f276
“steven1303” <steven.grafensen@gmail.com>
2023-01-08 14:08:39 +0700

    template

---
+++ src/resource/js/clipboard.js
@@ -0,0 +1,11 @@
+
+(function($) {
+  'use strict';
+  var clipboard = new ClipboardJS('.btn-clipboard');
+  clipboard.on('success', function(e) {
+    console.log(e);
+  });
+  clipboard.on('error', function(e) {
+    console.log(e);
+  });
+})(jQuery);
